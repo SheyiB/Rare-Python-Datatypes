aea5435e6bd18e80f41067d4ae291920c15beeca
Seyi-Banjo <elijahbanjo@gmail.com>
2023-01-30 16:51:25 +0100

    File Update

--- rare_data_types.py
+++ rare_data_types.py
@@ -1,44 +1,64 @@
 #Complex
 #Complex Computations
 cmplx = complex(1j)
-print(cmplx)
+print(cmplx + 2)
 print(type(cmplx))
 print("**********************************************")
 
+
 #List
 #Mutable, Changeable, Ordered Sequence, Multiple Items in a single Variable
-fruits_list = list(("dare", "funmi", "neeya", "funmi"))
-print(fruits_list)
-print(type(fruits_list))
+friends_list = list(("dare", "funmi", "neeya", "seyi"))
+for i in friends_list:
+    print(i)
+friends_list[2] = 'Ganeeya'
+print(friends_list)
+print(type(friends_list))
 print("**********************************************")
+
 
 #Tuple
 #Collection of Objects which are ordered and immutable
-fruits = tuple(("dare", "funmi", "neeya", "funmi"))
-print(fruits)
-print(type(fruits))
+friends = tuple(("dare", "funmi", "neeya", "jah", "lydia"))
+for i in friends:
+    print(i)
+#friends[2] = 'Ganeeya'
+print(friends)
+print(type(friends))
 print("**********************************************")
+
 
 #Range
 #A sequence of numbers starting from 0 and ends just before the specified number, increasing by 1 by default
 rng = range(6)
 print(rng)
+for i in rng:
+    print(i)
 print(type(rng))
 print("**********************************************")
 
 #Set
 #Unordered Collection of unique data that are iterable and mutable 
-my_set = {"abdul", "toyeeeb", "fadio", "fadio"}
+my_set = {"abdul", "toyeeb", "salam", "salam"}
+for i in my_set:
+    print(i)
+my_set.remove('toyeeb')
+my_set.add("Ibrahim")
 print(my_set)
 print(type(my_set))
 print("**********************************************")
 
+
 #Frozen Set
 #Immutable version of sets, cant change values, iterable
 my_frzn_st = frozenset({"dsa", "cybersecurity", "blockchain"})
+for i in my_frzn_st:
+    print(i)
+#my_frzn_st.remove('dsa')
 print(my_frzn_st)
 print(type(my_frzn_st))
 print("**********************************************")
+
 
 #Boolean
 #True or false in python, any value other than 0 will give true
@@ -47,12 +67,14 @@ print(my_bool)
 print(type(my_bool))
 print("**********************************************")
 
+
 #Byte
 #Converting to an immutable byte representation of a given data
-my_byte = b"Hack"
+my_byte = b"Lydia"
 print(my_byte)
 print(type(my_byte))
 print("**********************************************")
+
 
 #Byte Array
 #A mutable byte object
@@ -60,6 +82,7 @@ my_byte_array = bytearray(7)
 print(my_byte_array)
 print(type(my_byte_array))
 print("**********************************************")
+
 
 #Memory View
 #To show the internal data of an object. Supports buffer protocol. Yield large performance gains
